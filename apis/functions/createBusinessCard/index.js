8d3a930538f6ff3501e9f36373ef051420595842
iewil <liwei@data.gov.sg>
2018-06-02 15:38:28 +0800

    Refactor createBusinessCard lambda function to use aysnc await

--- apis/functions/createBusinessCard/index.js
+++ apis/functions/createBusinessCard/index.js
@@ -1,166 +1,126 @@
 const handlebars = require('handlebars');
 const randomBytes = require('crypto').randomBytes;
-const AWS = require('aws-sdk');
-const ddb = new AWS.DynamoDB.DocumentClient();
-const s3 = new AWS.S3();
+
 var Promise = require("bluebird");
 var fs = Promise.promisifyAll(require("fs"));
 var qr = require('qr-image');
 var vCard = require('vcards-js');
 const uuid = require('uuid/v4');
 
+const AWS = require('aws-sdk');
+
+// var credentials = new AWS.SharedIniFileCredentials({profile: 'digital-namecard'});
+// AWS.config.credentials = credentials;
+// AWS.config.update({region: 'ap-southeast-1'});
+
+const ddb = Promise.promisifyAll(new AWS.DynamoDB.DocumentClient());
+const s3 = Promise.promisifyAll(new AWS.S3());
 
 // Constants
 // =========
 const bucket = 'digital.business.card'
-const config = new AWS.Config({
-    accessKeyId: process.env.ACCESS_KEY_ID,
-    secretAccessKey: process.env.SECRET_ACCESS_KEY,
-    region: process.env.REGION
-});
-AWS.config.update(config);
+const templateFile = './assets/index.html';
+const bucketURL = 'http://digital.business.card.s3-website-ap-southeast-1.amazonaws.com/';
 
-console.log('starting function')
-
+// Test input for API Gateway
+// ==========================
+// var event = {
+//     "body": JSON.stringify({
+//         "full_name": "Preston Lim",
+//         "first_name": "Preston",
+//         "last_name": "Lim", 
+//         "role": "Associate Software Engineer", 
+//         "company": "Data Science Division, GovTech",
+//         "profile_photo": "[blob]",
+//         "company_logo": "[blob]",
+//         "actions": {
+//             "email": {
+//                 "value": "preston@data.gov.sg"
+//             },
+//             "phone_number": {
+//                 "value": "+65 9123 4567"
+//             },
+//             "website": {
+//                 "value": "https://tech.gov.sg"
+//             },
+//             "address": {
+//                 "address_street": "1 Fusionopolis, Sandcrawler, #09-01",
+//                 "address_city": "Singapore",
+//                 "address_stateProvince": "Singapore",
+//                 "address_postalCode": "138577",
+//                 "address_countryRegion": "Singapore"
+//             }
+//         }
+//     })
+// }
 
 // Lambda function proper
 // ======================
-exports.handle = (event, context, callback) => {
-    console.log('processing event')
-    console.log(event)
-    
-    // Test input for API Gateway
-    // ==========================
-    // {'full_name': 'Preston Lim',
-    //  'first_name': 'Preston',
-    //  'last_name': 'Lim', 
-    //  'role': 'Associate Software Engineer', 
-    //  'company': 'Data Science Division, GovTech',
-    //  'email': 'preston@data.gov.sg',
-    //  'phone_number': '+65 9123 4567',
-    //  'website': 'https://tech.gov.sg',
-    //  'address_street': '1 Fusionopolis, Sandcrawler, #09-01',
-    //  'address_city': 'Singapore',
-    //  'address_stateProvince': 'Singapore',
-    //  'address_postalCode': '138577',
-    //  'address_countryRegion': 'Singapore'
-    //  'profile_photo': [blob],
-    //  'company_logo': [blob],
-    // }
-    
-    // Generate UUIDs for eaach card and image group to prevent filename conflicts
-    const cardId = uuid();
-    console.log(cardId);
-    var json_object = JSON.parse(event.body)
-    var params = {'full_name': json_object.first_name + json_object.last_name,
-                    'first_name': json_object.first_name,
-                    'last_name': json_object.last_name, 
-                    'role': json_object.role, 
-                    'company': json_object.company,
-                    'email': json_object.actions.email.value,
-                    'phone_number': json_object.actions.phone_number.value,
-                    'website': json_object.actions.website.value,
-                    'address_street': json_object.actions.address.address_street,
-                    'address_city': json_object.actions.address.address_city,
-                    'address_stateProvince': json_object.actions.address.address_stateProvince,
-                    'address_postalCode': json_object.actions.address.address_postalCode,
-                    'address_countryRegion': json_object.actions.address.address_countryRegion,
-                    'address': json_object.actions.address.address_street + ", " + json_object.actions.address.address_city + ", " + json_object.actions.address.address_stateProvince + ", " + json_object.actions.address.address_postalCode + ", " + json_object.actions.address.address_countryRegion,
-                    'profile_photo': json_object.profile_photo,
-                    'company_logo': json_object.company_logo
-                    };
+exports.handle = async (event, context, callback) => {
+// main = async() => {
+    try {
+        console.log('start createBusinesCard...')
 
-    console.log("printing out params...")
-    console.log(params)
+        // Generate UUIDs for eaach card and image group to prevent filename conflicts
+        const cardId = 'test'; //uuid();
+        var s3_file_path = 'users/' + cardId
+        var params = parseRequestObject(JSON.parse(event.body));
 
-    const bucketURL = 'http://digital.business.card.s3-website-ap-southeast-1.amazonaws.com/';
-    var readFileName = './assets/index.html';
-    var mainHTMLName = 'users/' + cardId + '/index.html';
-    var qrFileName = 'users/' + cardId + '/qr.png';
-    var vcfName = 'users/' + cardId + '/user.vcf';
+        let files = await Promise.all([
+                        generateHTML(params),
+                        generateVCard(params),
+                        generateQRCode(cardId)])
 
-    var responseBody = {
-        path: bucketURL + mainHTMLName,
-    };
+        let results = await Promise.all([
+            uploadToS3( s3_file_path, 'index.html', 'html', files[0]), // files[0]: HTML
+            uploadToS3( s3_file_path, 'user.vcf', 'vcf', files[1]), // files[1]: VCard
+            uploadToS3( s3_file_path, 'qr.png', 'png', files[2]), // files[2]: QR Code
+            uploadToS3( s3_file_path, 'profile_photo.png', 'png', params['profile_photo']), // profile_photo
+            uploadToS3( s3_file_path, 'company_logo.png', 'png', params['company_logo']), // company_logo
+        ]);
 
-    const response = {
-        statusCode: 200,
-        headers: {
-            "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
-            "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS 
-        },
-        body: JSON.stringify(responseBody)
-    };
+        let recordId = await saveUserToDb(cardId, params);
 
-    // Step 1: Generate the static HTML website using the user input
-    var promise1 = renderTemplate(params, readFileName, mainHTMLName, bucket)
-    
-    // Step 2: Generate the VCF using the user input
-    var promise2 = generateVCard(params, vcfName, bucket);
-
-    // Step 3a: Upload profile and company photo to bucket
-    // var promise3ai = uploadImageToS3(cardId, 'profile.png', params.profile_photo);
-    // var promise3aii = uploadImageToS3(cardId, 'logo.png', params.company_logo);
-
-    // Step 3b: Generate the QR code file
-    var promise3b = generateQRCode(qrFileName, bucketURL + mainHTMLName);
-
-    // Step 4: Store the user input and file information into the dynamoDB table
-    // then return the CardId to the user/QR code IP address
-
-    Promise.all([promise1, promise2, promise3b])
-    .then(recordCardInfo(cardId, params))
-    .then(() => {
-        console.log('Record successfully added into dynamodb');
-        console.log("response: " + JSON.stringify(response))
-        callback(null, response);
-    }).catch((err) => {
-        console.log('DynamoDB error');
-        console.error(err);
-        errorResponse(err.message, params.awsRequestId, callback);
-    });
+    } catch (err) {
+        console.log("failed to createBusinessCard: ", err)
+    }
 };
 
-function uploadToS3(fileName, body, isHTML) {
-    if (isHTML) {
-        var params = {Bucket: bucket, Key: fileName, Body: body, ContentType: 'text/html'}; 
-    } else {
-        var params = {Bucket: bucket, Key: fileName, Body: body};
-    }
+// main()
 
-    s3.upload(params, function(err, data) {
-        console.log(err, data);
-        console.log('File successfully uploaded to: ' + data.Location);
-    });
-}
 
-function uploadImageToS3(cardId, fileName, imageBinary) {
-    return new Promise((resolve, reject) => {
-        console.log("uploading images to s3")
-        buf = new Buffer(imageBinary.replace(/^data:image\/\w+;base64,/, ""),'base64')
+
+async function uploadToS3(s3_file_path, s3_file_name, file_type, file_body) {
+    console.log("Start uploading file to s3: ", s3_file_path, "; ", s3_file_name, "; ", file_type)
+    try {
         var params = {
-            Bucket: bucket,
-            Key: 'users/' + cardId + '/' + fileName, 
-            Body: buf,
-            ContentEncoding: 'base64',
-            ContentType: 'image/png'
-        };
-        s3.putObject(params, function(err, data){
-            if (err) { 
-                console.log(err);
-                console.log('Error uploading data: ', data); 
-            } else {
-                console.log('succesfully uploaded the image!');
-            }
-        });
-    })
+            Bucket: bucket, 
+            Key: s3_file_path + '/' + s3_file_name, 
+            Body: file_body, 
+        }; 
+        if (file_type == "html") {
+            params["ContentType"] = 'text/html'
+        } else if (file_type == "png") {
+            params["ContentEncoding"] = 'base64'
+            params["ContentType"] = 'image/png'
+        } else if (file_type == "vcf") {
+            params["ContentType"] = 'text/x-vcard'
+        }
+        let uploadFile = await s3.uploadAsync(params);
+        console.log("uploadToS3: Sucess!")
+        return Promise.resolve(uploadFile)
+
+    } catch (err) {
+        console.log("Failed to upload file to s3: ", err)
+        return Promise.reject(err)
+    }
 }
 
-// Generate the VCF using 'vcf'
-function generateVCard(params, vcfName){
-    return new Promise((resolve, reject) => {
+// Generate VCF file
+async function generateVCard(params) {
+    console.log("Start generating VCard...")
+    try {
         vCard = vCard();
-
         //set properties
         vCard.firstName = params['first_name'];
         vCard.lastName = params['last_name'];
@@ -177,65 +137,109 @@ function generateVCard(params, vcfName){
         vCard.workAddress.stateProvince = params['address_stateProvince'];
         vCard.workAddress.postalCode = params['address_postalCode'];
         vCard.workAddress.countryRegion = params['address_countryRegion'];
-
-        console.log(vCard.getFormattedString());
-
-        uploadToS3(vcfName, vCard.getFormattedString(), false);
-    });
+        console.log("generateVCard: Sucess!")
+        return new Promise.resolve(vCard.getFormattedString());
+    } catch (err) {
+        console.log("Failed to generate VCard: ", err)
+        return new Promise.reject(err)
+    }
 }
 
-// Use handlebars to render the HTML from template; returns a Promise
-function renderTemplate(params, readFileName, writeFileName, bucket) {
-    return new Promise((resolve, reject) => {
-        fs.readFileAsync(readFileName)
-        .then((data) => {
-            var source = data.toString('utf8'); // To convert the buffer stream into a string
-            var template = handlebars.compile(source); // Handlebars at work
-            var renderedTemplate = template(params);
-            uploadToS3(writeFileName, renderedTemplate, true);
-        });
-    });
+// Generate HTML file
+async function generateHTML(params) {
+    console.log("Start generating HTML...")
+    try {
+        var template_file = await fs.readFileAsync(templateFile);
+        var template = template_file.toString('utf8');
+        var templatin_engine = handlebars.compile(template);
+        var rendered_template = templatin_engine(params);
+        console.log("generateHTML: Sucess!")
+        return new Promise.resolve(rendered_template);
+    } catch (err) {
+        console.log("Failed to generate HTML: ", err)
+        return new Promise.reject(err)
+    }
 }
 
-function generateQRCode(qrFileName, cardURL) {
-    return new Promise((resolve, reject) => {
-        var qr_stream = qr.image(cardURL, { ec_level: 'H' });
-        uploadToS3(qrFileName, qr_stream, true);  
-    });
+// Generate QR Code 
+async function generateQRCode(cardId) {
+    console.log("Start generating QR Code...")
+    try {
+        var cardURL = bucketURL + 'users/' + cardId + '/qr.png'
+        var qr_code = qr.image(cardURL, { ec_level: 'H' });
+        console.log("generateQRCode: Sucess!")
+        return Promise.resolve(qr_code)
+    } catch (err) {
+        console.log("Failed to generate QR Code: ", err)
+        return new Promise.reject(err)
+    }
 }
 
 // Put the user input into dynamoDB
-function recordCardInfo(cardId, params) {
-    console.log('Begin recording card info into dynamoDB')
-    return ddb.put({
-        TableName: 'DigitalBusinessCards',
-        Item: {
-            CardId: cardId,
-            Info: params,
-            RequestTime: new Date().toISOString(),
-        },
-    }).promise();
-}
+async function saveUserToDb(cardId, params) {
+    console.log('Start recording card info into dynamoDB')
+    try {
+        let saveUser = await ddb.putAsync({
+            TableName: 'DigitalBusinessCards',
+            Item: {
+                CardId: cardId,
+                Info: params,
+                RequestTime: new Date().toISOString(),
+            },
+        })
+        console.log("saveUserToDb: Sucess!")
+        return Promise.resolve(saveUser)
+    } catch (err) {
+        console.log("Failed to save user into dynamodb: ", err)
+        return Promise.reject(err)
+    };
+};
 
-
-// Generate a usable URL string from the random bytes
-function toUrlString(buffer) {
-    return buffer.toString('base64')
-        .replace(/\+/g, '-')
-        .replace(/\//g, '_')
-        .replace(/=/g, '');
+function parseRequestObject(request_object) {
+    return {
+        'full_name': request_object.first_name + request_object.last_name,
+        'first_name': request_object.first_name,
+        'last_name': request_object.last_name, 
+        'role': request_object.role, 
+        'company': request_object.company,
+        'email': request_object.actions.email.value,
+        'phone_number': request_object.actions.phone_number.value,
+        'website': request_object.actions.website.value,
+        'address_street': request_object.actions.address.address_street,
+        'address_city': request_object.actions.address.address_city,
+        'address_stateProvince': request_object.actions.address.address_stateProvince,
+        'address_postalCode': request_object.actions.address.address_postalCode,
+        'address_countryRegion': request_object.actions.address.address_countryRegion,
+        'address': request_object.actions.address.address_street + ", " + request_object.actions.address.address_city + ", " + request_object.actions.address.address_stateProvince + ", " + request_object.actions.address.address_postalCode + ", " + request_object.actions.address.address_countryRegion,
+        'profile_photo': request_object.profile_photo,
+        'company_logo': request_object.company_logo
+    };
 }
 
 // Return a helpful error message
-function errorResponse(errorMessage, awsRequestId, callback) {
-  callback(null, {
-    statusCode: 500,
-    body: JSON.stringify({
-      Error: errorMessage,
-      Reference: awsRequestId,
-    }),
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-    },
-  });
-}
+function errorResponse(callback, cardId, errorMessage) {
+    callback(null, {
+        statusCode: 500,
+        headers: {
+            "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
+            "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS 
+        },
+        body: JSON.stringify({
+            error: errorMessage,
+            path: bucketURL + 'users/' + cardId + '/index.html',
+        })
+    });
+};
+
+function successResponse(callback, cardId) {
+    callback(null, {
+        statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
+            "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS 
+        },
+        body: JSON.stringify({
+            path: bucketURL + 'users/' + cardId + '/index.html',
+        })
+    })
+};
